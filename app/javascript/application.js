7ebc084f8752edba2c74c6c9c2aedacbf7aa757a
kanata-eng1225 <murasanv1507@gmail.com>
2023-09-04 21:58:52 +0900

    オススメのストレス解消法の掲示板の星の修正コードを元に戻す

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,24 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-document.addEventListener("turbo:load", function() {
-  document.querySelectorAll('.star-label').forEach((label, index, labelArray) => {
-    label.addEventListener('click', (e) => {
-      const radioButton = document.getElementById(`rating_${index + 1}`);
-      radioButton.checked = true;
-
-      labelArray.forEach((starLabel, starIndex) => {
-        if (starIndex <= index) {
-          starLabel.textContent = "★";
-          starLabel.classList.remove('text-gray-400');
-          starLabel.classList.add('text-yellow-500');
-        } else {
-          starLabel.textContent = "★";
-          starLabel.classList.remove('text-yellow-500');
-          starLabel.classList.add('text-gray-400');
-        }
-      });
-    });
-  });
-});
+import "./rating"
