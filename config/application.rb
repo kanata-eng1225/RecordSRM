6fa09da643a800372a2e7dc7e991b3eda4ba1132
kanata-eng1225 <murasanv1507@gmail.com>
2023-09-16 23:50:56 +0900

    国際化

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module RecordSRM
 
     config.time_zone = 'Tokyo'
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
