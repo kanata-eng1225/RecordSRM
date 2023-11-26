90cf66dc77830d03e5b195b573bcca3ec72dc273
kanata-eng1225 <murasanv1507@gmail.com>
2023-11-27 00:08:45 +0900

    レコメンド機能追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,27 +1,20 @@
 class LikesController < ApplicationController
-  before_action :store_location, only: %i[create destroy]
-
   def create
     @like = current_user.likes.build(stress_relief_id: params[:stress_relief_id])
     @like.save
-    redirect_after_like
+    render_like_partial
   end
 
   def destroy
     @like = current_user.likes.find_by(stress_relief_id: params[:stress_relief_id])
     @like.destroy
-    redirect_after_like
+    render_like_partial
   end
 
   private
 
-  def store_location
-    # セッションに元いたページを保存
-    session[:return_to] = request.referer
-  end
-
-  def redirect_after_like
-    # セッションに保存されたページにリダイレクト、なければデフォルトのページにリダイレクト
-    redirect_to session.delete(:return_to) || stress_reliefs_path
+  def render_like_partial
+    @stress_relief = StressRelief.find(params[:stress_relief_id])
+    render partial: 'like_button', locals: { stress_relief: @stress_relief }
   end
 end
